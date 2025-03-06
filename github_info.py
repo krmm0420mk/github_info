3baf0d93fd7ce16fb43e2d88924c391e6b7728e2
krmm0420mk <Mrakolister@protonmail.com>
2025-03-06 07:39:45 -0500

    Create github_info.py

---
+++ github_info.py
@@ -0,0 +1,18 @@
+import requests
+
+BASE_URL = "https://api.github.com/users/"
+
+def get_github_user(username):
+    response = requests.get(BASE_URL + username)
+    data = response.json()
+    
+    if response.status_code == 200:
+        print(f"Name: {data.get('name', 'N/A')}")
+        print(f"Public Repos: {data.get('public_repos', 0)}")
+        print(f"Followers: {data.get('followers', 0)}")
+    else:
+        print("Error:", data["message"])
+
+if __name__ == "__main__":
+    username = input("Enter GitHub username: ")
+    get_github_user(username)
